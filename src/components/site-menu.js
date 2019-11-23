fe794607a676df5b20b577d3930569ad98628646
Юрий <realainov@ya.ru>
2019-11-23 17:48:21 +0300

    Добавляет модули в проект

---
+++ src/components/site-menu.js
@@ -0,0 +1,12 @@
+const createTemplate = () => {
+  return (
+    `<nav class="trip-controls__trip-tabs  trip-tabs">
+      <a class="trip-tabs__btn  trip-tabs__btn--active" href="#">Table</a>
+      <a class="trip-tabs__btn" href="#">Stats</a>
+    </nav>`
+  );
+};
+
+export const siteMenu = {
+  createTemplate
+};
